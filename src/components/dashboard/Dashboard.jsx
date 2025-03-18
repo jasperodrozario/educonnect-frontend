26e485709b682d59a953072e89421195b506db50
Jasper D Rozario <jasper.odrozario@gmail.com>
2025-03-18 10:26:48 +0600

    Update Dashboard.jsx

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -101,7 +101,7 @@ export function Dashboard() {
           <span className="p-0 m-0 text-xs text-neutral-400 font-semibold">
             {displayedRooms.length} rooms available
           </span>
-<button type="button" className="w-20 p-4 font-semibold my-2">
+<button type="button" className="w-20 p-4 font-semibold my-2 bg-orange-600">
 + Create Room
 </button>
         </div>
