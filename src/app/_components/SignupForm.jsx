cd78de8b25be19145ec5ae0c771d884e1c8420ca
Jasper D Rozario <jasper.odrozario@gmail.com>
2025-03-18 09:40:38 +0600

    Update SignupForm.jsx

--- src/app/_components/SignupForm.jsx
+++ src/app/_components/SignupForm.jsx
@@ -84,9 +84,9 @@ export function SignupForm() {
             className="group/btn shadow-input relative flex h-10 w-full items-center justify-start space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626]"
             type="submit"
           >
-            <IconBrandOnlyfans className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+            <IconBrandX className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
             <span className="text-sm text-neutral-700 dark:text-neutral-300">
-              OnlyFans
+              X
             </span>
             <BottomGradient />
           </button>
